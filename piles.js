d662003cac75906a11eb054e35a7aa60f0ae5831
aashishword004-alt <aashishword004@gmail.com>
2026-08-10 20:29:39 +0530

    done with mvc

--- piles.js
+++ piles.js
@@ -5,4 +5,21 @@ There are a number of piles arranged in a row, and each pile
 
 Alice and Bob take turns, with Alice starting first. */
 
+// let piles = [5,3,2,1]
+// function stoneGame() {
 
+//     let bob = params(' take the stones');
+//     let alice = params(' take the stones');
+  
+//     console.log('Bob took ' + bob + ' stones');
+//     console.log('Alice took ' + alice + ' stones');
+
+// }
+
+// stoneGame(piles);
+
+let prompt = require('prompt-sync')();
+
+let bob = prompt();
+
+console.log('Bob took ' + bob + ' stones');
